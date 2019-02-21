81b344092dbd32a29e039e5077b70c55a22d857e
Alex Petrenko <a.petrenko@rczifort.local>
2019-02-21 18:13:34 +0300

    step five

--- tinypro/tinypro.cpp
+++ tinypro/tinypro.cpp
@@ -1,3 +1,6 @@
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
+
 #include "test_runner.h"
 #include "profile.h"
 #include <iostream>
@@ -68,22 +71,23 @@ int main() {
 
     assert(sizeof(map) == map_w * map_h + 1u); // +1 for the null terminated string
     // Add player position
-    float player_x = 3.456; // player x position
-    float player_y = 2.345; // player y position
-    float player_a = 1.523; // player view direction
+    float player_x = 3.456;         // player x position
+    float player_y = 2.345;         // player y position
+    float player_a = 1.523;         // player view direction
+    const float fov = M_PI / 3.;    // field of view
 
-    for (size_t j = 0; j < win_h; j++) { // fill the screen with color gradients
+    for (size_t j = 0; j < win_h; j++) {        // fill the screen with color gradients
         for (size_t i = 0; i < win_w; i++) {
             uint8_t r = 255 * j / float(win_h); // varies between 0 and 255 as j sweeps the vertical
             uint8_t g = 255 * i / float(win_w); // varies between 0 and 255 as i sweeps the horizontal
             uint8_t b = 0;
             framebuffer[i + j * win_w] = pack_color(r, g, b);
         }
-    } 
+    }   
 
     const size_t rect_w = win_w / map_w;
     const size_t rect_h = win_h / map_h;
-    for (size_t j = 0; j < map_h; j++) { // draw the map
+    for (size_t j = 0; j < map_h; j++) {        // draw the map
         for (size_t i = 0; i < map_w; i++) {
             if (map[i + j * map_w] == ' ') continue; // skip empty spaces
             size_t rect_x = i * rect_w;
@@ -95,15 +99,19 @@ int main() {
     // draw the player on the map
     draw_rectangle(framebuffer, win_w, win_h, player_x*rect_w, player_y*rect_h, 5, 5, pack_color(255, 255, 255));
 
-    // trace of the first ray
-    for (float t = 0; t < 20; t += .05) {
-        float cx = player_x + t * cos(player_a);
-        float cy = player_y + t * sin(player_a);
-        if (map[int(cx) + int(cy)*map_w] != ' ') break;
+    for (size_t i = 0; i < win_w; i++) {        // draw the visibility cone
+        float angle = player_a - fov / 2 + fov * i / float(win_w);
 
-        size_t pix_x = cx * rect_w;
-        size_t pix_y = cy * rect_h;
-        framebuffer[pix_x + pix_y * win_w] = pack_color(255, 255, 255);
+        // trace of the first ray
+        for (float t = 0; t < 20; t += .05) {
+            float cx = player_x + t * cos(angle);
+            float cy = player_y + t * sin(angle);
+            if (map[int(cx) + int(cy)*map_w] != ' ') break;
+
+            size_t pix_x = cx * rect_w;
+            size_t pix_y = cy * rect_h;
+            framebuffer[pix_x + pix_y * win_w] = pack_color(255, 255, 255);
+        }
     }
 
     drop_ppm_image("./out.ppm", framebuffer, win_w, win_h);
